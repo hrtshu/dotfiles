2b264791c61041d508125a0780e42918fb19985f
Shuhei <sh7916@gmail.com>
2023-12-28 12:45:02 +0900

    improve password notification handling

--- get-login-pass.sh
+++ get-login-pass.sh
@@ -22,8 +22,18 @@ elif [ -x /usr/bin/terminal-notifier ]; then
     NOTIFIER=/usr/bin/terminal-notifier
 fi
 
+NOTIFICATION_GROUP_ID="$(uuidgen || echo get-login-pass)"
+
 DIALOG_TITLE="${1-password}"
 
-[ -n "$NOTIFIER" ] && "$NOTIFIER" -title "Password Required" -message "Please enter your password." -sound default
-pass="$("$ZENITY" --password --title="$DIALOG_TITLE")" || exit 2
-echo "$pass"
+[ -n "$NOTIFIER" ] && "$NOTIFIER" -group "$NOTIFICATION_GROUP_ID" -title "Password Required" -message "Please enter your password." -sound default
+
+EXIT_CODE=0
+if pass="$("$ZENITY" --password --title="$DIALOG_TITLE")"; then
+    echo "$pass"
+else
+    EXIT_CODE=2
+fi
+
+[ -n "$NOTIFIER" ] && "$NOTIFIER" -remove "$NOTIFICATION_GROUP_ID" >/dev/null 2>&1
+exit $EXIT_CODE
